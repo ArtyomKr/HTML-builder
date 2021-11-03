f7f5fe13ef23e16038d471d3565ef5844d1d0142
ArtyomKr <ArtyomKr@users.noreply.github.com>
2021-11-03 20:27:52 +0300

    fix: improved algorithm for copying files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,16 +4,30 @@ const fsPromises = require('fs').promises;
 const folderToCopy = path.join(__dirname, 'files');
 const copiedFolder = path.join(__dirname, 'files-copy');
 
-
 fsPromises.mkdir(copiedFolder).then(() => {
   console.log('Directory created successfully');
-}).catch(() => {
-  console.log('Directory already exists');
-});
+}).catch(() => {});
 
 fsPromises.readdir(folderToCopy).then(files => {
+  //deleting files that were deleted in original folder
+  fsPromises.readdir(copiedFolder).then(copiedFiles => {
+    copiedFiles.forEach(copiedFile => {
+      const fileExists = files.includes(copiedFile);
+      if (!fileExists) fsPromises.unlink(`${copiedFolder}\\${copiedFile}`);
+    });
+  });
+
   files.forEach(async file => {
-    fsPromises.copyFile(`${folderToCopy}\\${file}`, `${copiedFolder}\\${file}`)
-      .catch(err => console.log(err))
+    //checking last time when files were modified
+    let mTimeCopied =  await fsPromises.stat(`${copiedFolder}\\${file}`)
+      .then(stats => stats.mtimeMs)
+      .catch(() => null); //if file is new
+    let mTimeOriginal = await fsPromises.stat(`${folderToCopy}\\${file}`).then(stats => stats.mtimeMs);
+
+    //files are copying only if they were modified
+    if (mTimeOriginal !== mTimeCopied) {
+      fsPromises.copyFile(`${folderToCopy}\\${file}`, `${copiedFolder}\\${file}`)
+        .catch(err => console.log(err));
+    }
   });
 });

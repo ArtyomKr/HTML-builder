d3daf6a883bccd7b120053fa5020f4659cd24e89
ArtyomKr <ArtyomKr@users.noreply.github.com>
2021-11-03 18:54:27 +0300

    feat: make a copy-directory function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const path = require('path');
+const fsPromises = require('fs').promises;
 
+const folderToCopy = path.join(__dirname, 'files');
+const copiedFolder = path.join(__dirname, 'files-copy');
+
+
+fsPromises.mkdir(copiedFolder).then(() => {
+  console.log('Directory created successfully');
+}).catch(() => {
+  console.log('Directory already exists');
+});
+
+fsPromises.readdir(folderToCopy).then(files => {
+  files.forEach(async file => {
+    fsPromises.copyFile(`${folderToCopy}\\${file}`, `${copiedFolder}\\${file}`)
+      .catch(err => console.log(err))
+  });
+});
